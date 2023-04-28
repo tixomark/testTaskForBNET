a959f29152792e2694b8249bbb5edbed3750c4f8
tixomark <tixomark@mail.ru>
2023-04-28 10:05:44 +0300

    initial commit

---
+++ testTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testTask
+//
+//  Created by tixomark on 4/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
